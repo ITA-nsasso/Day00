2e2fd5ad32f2384911d2836378906ab0420df98f
ITA-nsasso <n.sasso@it-students.fr>
2021-09-13 16:30:46 +0200

    Finish ex07

--- ex07/ledger.cpp
+++ ex07/ledger.cpp
@@ -23,10 +23,33 @@ int main(){
 
     cout << "Quantité Dogecoins : ";
     cin >> dogecoin;
+    dogecoin *= 0.2;
 
     cout << "Quantité Cardano : ";
     cin >> cardano;
+    cardano *= 2;
 
+    cout << "\n";
 
+    cout << "Bitcoin : ";
+    cout << bitcoin;
+    cout << "\n";
 
+    cout << "Ethereum : ";
+    cout << ethereum;
+    cout << "\n";
+
+    cout << "XRP : ";
+    cout << xrp;
+    cout << "\n";
+
+    cout << "Dogecoin : ";
+    cout << dogecoin;
+    cout << "\n";
+
+    cout << "Cardano : ";
+    cout << cardano;
+    cout << "\n";
+
+    return 0;
 }
